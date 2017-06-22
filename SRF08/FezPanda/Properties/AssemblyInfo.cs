83c90035a4c039910e41e7ab0eb83c8fb649c8bf
Mariano Philippe <philippemariano@hotmail.fr>
2017-06-22 17:40:54 +0200

    Update v1.4.5.1

--- SRF08/FezPanda/Properties/AssemblyInfo.cs
+++ SRF08/FezPanda/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FEZMFApplication1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("FezPanda")]
+[assembly: AssemblyDescription("SRF08 class test")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FEZMFApplication1")]
+[assembly: AssemblyProduct("GHI Fez Panda III")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
